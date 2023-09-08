945d2baa0a082e9b873d8876c29e631e9ec7d014
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-08 16:43:21 +0600

    Assignment of Mod_5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp (MyApp());
+void main(){
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: HomeScreen(),
+      title: 'OSTAD APP',
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        toolbarHeight: 100,
+        elevation: 70,
+        leading: Icon(Icons.store),
+        actions: [
+          Icon(Icons.search)
+        ],
+        centerTitle: true,
+        title: Text('Home'),
+        backgroundColor: Colors.green,
+
+      ),
+
+
+
+    );
+  }
+}
