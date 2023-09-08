10a84ad34dd17c9bd1038b04a6296b841cd33fd8
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-08 17:55:11 +0600

    Assignment of Mod_5

--- lib/main.dart
+++ lib/main.dart
@@ -26,18 +26,68 @@ class HomeScreen extends StatelessWidget{
       appBar: AppBar(
         toolbarHeight: 100,
         elevation: 70,
-        leading: Icon(Icons.store),
+        leading: Icon(Icons.store, size: 36),
         actions: [
-          Icon(Icons.search)
+          Icon(Icons.search, size: 36)
         ],
         centerTitle: true,
-        title: Text('Home'),
+        title: Text('Home', style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 26
+        ),),
         backgroundColor: Colors.green,
 
       ),
 
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('This is mod 5 Assignment', style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              ),)
 
+            ],
 
+          ),
+
+          RichText(text: TextSpan(
+            text: 'My ',
+            style: TextStyle(
+                color: Colors.pink,
+                fontSize: 28
+            ),
+
+              children: [
+              TextSpan(
+                text: 'phone ',
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 18
+                )
+              ),
+              TextSpan(
+                text: 'name ',
+                style: TextStyle(
+                    color: Colors.purpleAccent,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold
+                )
+              ),
+              TextSpan(
+                text: 'Your phone name',
+                style: TextStyle(
+                  color: Colors.orange
+                )
+              )
+            ]
+
+          ))
+        ],
+      )
     );
   }
 }
