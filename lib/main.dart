8c9835cd254517152af0d69439ff6a22e2514f47
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-09-08 17:57:49 +0600

    Assignment of Mod_5

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       home: HomeScreen(),
-      title: 'OSTAD APP',
+      title: 'Assignment APP',
     );
   }
 }
